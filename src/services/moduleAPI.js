eeeef879abf4446b426e4bcee549f66879790b7b
Divya123456Roy <divyaroy1947@gmail.com>
2025-06-25 16:45:31 +0530

    loo

--- src/services/moduleAPI.js
+++ src/services/moduleAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { BASE_URL } from "../utils/urls";
-import { getToken } from "../Utils/StorageHandler";
+import { getToken } from "../utils/storageHandler";
 
 
 // Fetch modules by course ID
